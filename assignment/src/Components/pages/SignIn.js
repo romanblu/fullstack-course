bdb7b995de27081e6a1441a55fbc6cc0a186a08d
roman2702rb <roman2702rb@gmail.com>
2021-04-19 17:04:43 +0300

    added login page assignment

--- assignment/src/Components/pages/SignIn.js
+++ assignment/src/Components/pages/SignIn.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import {Button, Input, Form, Segment, Grid, Header} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class SignIn extends React.Component {
     render () {
         return (
-            <h1>SIGN IN </h1>
+            <>
+                <div>
+                    <Grid verticalAlign="middle" centered columns={2} style={{height:"100vh" }}>
+                        <Grid.Column style={{maxWidth:"450px" }}>
+                            <Header as="h1" textAlign='center'>
+                                Login
+                            </Header>
+                            
+                            <Form centered>
+                                <Segment raised stacked>
+                                    <Form.Field>
+                                        <Input focut placeholder="Email" />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <Input focut placeholder="Password" />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <Button fluid type="submit">Login</Button>
+                                    </Form.Field>
+                                    <Grid>
+                                        <Grid.Column textAlign="center">
+                                            <Link to="/">Forgot Username/Password</Link>
+                                        </Grid.Column>
+                                    </Grid>
+                                    
+                                </Segment>
+                            </Form>
+                        </Grid.Column>
+                    </Grid>
+                </div>
+            </>
         );
     }
 }
